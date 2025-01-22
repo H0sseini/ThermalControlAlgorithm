ed10668b08d8cd73de9dfa7c5bed5a59e953fa76
H0sseini <59258888+H0sseini@users.noreply.github.com>
2025-01-21 23:27:16 -0800

    Update ThermalControlAlgorithm.cpp

--- cpp/ThermalControlAlgorithm/ThermalControlAlgorithm.cpp
+++ cpp/ThermalControlAlgorithm/ThermalControlAlgorithm.cpp
@@ -1,19 +1,22 @@
 
-//#include <iostream>
+#include <iostream>
 #include <string>
 //#include <chrono>
 #include <thread>
 #include "TCSInit.h"
 
-constexpr auto TCS_RESET_TIME = 20;	// seconds to reset everything in TCS
-constexpr auto TCS_RUN_INTERVAL = 10; //seconds to run the tcs code
+constexpr auto TCS_RESET_TIME = 1800;	// seconds to reset everything in TCS
+extern "C" {
+	__declspec(dllexport) constexpr auto TCS_RUN_INTERVAL = 10; //seconds to run the tcs code. this parameter is sent to c# for visualization)
+}
+
 
 int recordSize = sizeof(CompsData);
 int numRecords = comps_bin_len / recordSize;
 vector<TCSComponents>			tcsComponents(numRecords);
+vector<TCSComponents>			tcsBackup(numRecords);
 
 using namespace std;
-
 
 
 void TCS(vector<TCSComponents>& tcsComponents)
@@ -32,24 +35,19 @@ void TCS(vector<TCSComponents>& tcsComponents)
 	*/
 
 	
-
-	
-	//Check to see everything is ok
-	cout << tcsComponents[1].MaxOpTemp << endl;
-	cout << tcsComponents[5].sensor[2].IsDefined << endl;
-	cout << tcsComponents[4].heater[0].HeaterPIDNumber << endl;
-
-	
 	for (int comps = 0; comps < numRecords; comps++)
 	{
-		tcsComponents[comps].getTemps();
-		tcsComponents[comps].Temperature = tcsComponents[comps].setTemperature();
+		tcsComponents[comps].getTemps(); //getting values from sensors
+		tcsComponents[comps].Temperature = tcsComponents[comps].setTemperature(); //analyzing and setting temperatures for components
+		
 
 		tcsComponents[comps].AlgorithmBypass = tcsComponents[comps].checkBypass();
 		if (!tcsComponents[comps].AlgorithmBypass)
 		{
-			tcsComponents[comps].decideOnTemperature();
-			tcsComponents[comps].checkHeater();
+			tcsComponents[comps].decideOnTemperature(); //check to see whether anything should be done or not
+			// if component has heaters, the decision about turning on/off heaters sending RIDs and turning off component is made in the next step
+			// (chechHeater) and here just the status quo of the component is decided. Otherwise (no heaters), RIDs are sent here.
+			tcsComponents[comps].checkHeater(); // making decision about the status of heaters and corresponding component
 
 		}
 		else
@@ -57,86 +55,11 @@ void TCS(vector<TCSComponents>& tcsComponents)
 			// Do nothing. just report the temperature
 		}
 	}
-	cout << "TCS called" << endl;
+	
+	//cout << "TCS called" << endl;
 	
 }
 
-
-extern "C" __declspec(dllexport) int GetXValue(int Comp, int index)
-{
-	/*
-	* Guidlines for sending values to GUI:
-	* 1) The first input is the number of component
-	* check the numbers according the combo_box in c# or the CompsNames in TCSInit.h
-	* 2) second input is the index to identify which parameter should be sent:
-	* there are a total of 41 parameters in the GUI: 15 for components, 18 for sensors
-	* (6 for each), and 8 for heaters (4 for each).they are sent to the gui according to their presence
-	* in the GUI. It means that first component properties are sent and then, sensor1, sensor2, sensor3,
-	* heater1 and at last heater2. For each section, the order is from top to bottom.
-	*/
-	int value = -27300;
-
-	if (Comp >= 0 && Comp < numRecords)
-	{
-		switch (index)
-		{
-			//Component properties
-		case 0:
-			value = tcsComponents[Comp].ComponentPIDNumber;
-			break;
-		case 1:
-			value = tcsComponents[Comp].Temperature;
-			break;
-		case 2:
-			value = tcsComponents[Comp].Status;
-			break;
-		case 3:
-			value = tcsComponents[Comp].MaxOpTemp;
-			break;
-		case 4:
-			value = tcsComponents[Comp].MinOpTemp;
-			break;
-		case 5:
-			value = tcsComponents[Comp].MaxNopTemp;
-			break;
-		case 6:
-			value = tcsComponents[Comp].MinNopTemp;
-			break;
-		case 7:
-			value = tcsComponents[Comp].StartHeatOp;
-			break;
-		case 8:
-			value = tcsComponents[Comp].StartHeatOp;
-			break;
-		case 9:
-			value = tcsComponents[Comp].StartHeatNop;
-			break;
-		case 10:
-			value = tcsComponents[Comp].StartHeatNop;
-			break;
-		case 11:
-			value = tcsComponents[Comp].ValidSensors;
-			break;
-		case 12:
-			value = tcsComponents[Comp].ValidHeater;
-			break;
-		case 13:
-			value = tcsComponents[Comp].OBC;
-			break;
-		case 14:
-			value = tcsComponents[Comp].AlgorithmBypass;
-			break;
-			//Sensor 1 properties
-		default:
-			break;
-		}
-	}
-	else
-	{
-		//error selecting index in the combo box so the value is not changed
-	}
-	return value;
-}
 
 extern "C" __declspec(dllexport) int getComponentLength()
 {
@@ -145,7 +68,7 @@ extern "C" __declspec(dllexport) int getComponentLength()
 
 }
 
-extern "C" __declspec(dllexport) const char* GetObjectName(int index)
+extern "C" __declspec(dllexport) const char* getObjectName(int index)
 {
 
 	if (index >= 0 && index < numRecords)
@@ -155,30 +78,397 @@ extern "C" __declspec(dllexport) const char* GetObjectName(int index)
 	return nullptr; // Return null if index is invalid
 }
 
-extern "C" __declspec(dllexport) void mainFunc()
+/*
+	Sending component data from c++ to c# using functions.
+*/
+extern "C" 
+{
+	__declspec(dllexport) int sendCompPID(int index)
+	{
+		return tcsComponents[index].ComponentPIDNumber;
+	}
+	__declspec(dllexport) int sendCompTemp(int index)
+	{
+		return tcsComponents[index].Temperature;
+	}
+	__declspec(dllexport) int sendCompStatus(int index)
+	{
+		return tcsComponents[index].Status;
+	}
+	__declspec(dllexport) int sendCompMaxOpTemp(int index)
+	{
+		return tcsComponents[index].MaxOpTemp;
+	}
+	__declspec(dllexport) int sendCompMinOpTemp(int index)
+	{
+		return tcsComponents[index].MinOpTemp;
+	}
+	__declspec(dllexport) int sendCompMaxNopTemp(int index)
+	{
+		return tcsComponents[index].MaxNopTemp;
+	}
+	__declspec(dllexport) int sendCompMinNopTemp(int index)
+	{
+		return tcsComponents[index].MinNopTemp;
+	}
+	__declspec(dllexport) int sendCompStartHeatOp(int index)
+	{
+		return tcsComponents[index].StartHeatOp;
+	}
+	__declspec(dllexport) int sendCompStopHeatOp(int index)
+	{
+		return tcsComponents[index].StopHeatOp;
+	}
+	__declspec(dllexport) int sendCompStartHeatNop(int index)
+	{
+		return tcsComponents[index].StartHeatNop;
+	}
+	__declspec(dllexport) int sendCompStopHeatNop(int index)
+	{
+		return tcsComponents[index].StopHeatNop;
+	}
+	__declspec(dllexport) int sendValidSensor(int index)
+	{
+		return tcsComponents[index].ValidSensors;
+	}
+	__declspec(dllexport) int sendCompValidHeater(int index)
+	{
+		return tcsComponents[index].ValidHeaters;
+	}
+	__declspec(dllexport) int sendCompOBC(int index)
+	{
+		return tcsComponents[index].OBCCommand;
+	}
+	__declspec(dllexport) int sendCompBypass(int index)
+	{
+		return tcsComponents[index].AlgorithmBypass;
+	}
+	__declspec(dllexport) int sendCompTCSState(int index)
+	{
+		return tcsComponents[index].TCSState;
+	}
+}
+/*
+	Sending sensor 1 data from c++ to c# 
+*/
+extern "C"
+{
+	__declspec(dllexport) int sendSens1Defined(int index)
+	{
+		return tcsComponents[index].sensor[0].IsDefined;
+	}
+	__declspec(dllexport) int sendSens1PID(int index)
+	{
+		return tcsComponents[index].sensor[0].SensorPIDNumber;
+	}
+	__declspec(dllexport) int sendSens1State(int index)
+	{
+		return tcsComponents[index].sensor[0].State;
+	}
+	__declspec(dllexport) int sendSens1OnlineVal(int index)
+	{
+		return tcsComponents[index].sensor[0].OnlineValue;
+	}
+	__declspec(dllexport) int sendSens1PrevVal(int index)
+	{
+		return tcsComponents[index].sensor[0].PreviousValue;
+	}
+	__declspec(dllexport) int sendSens1Fault(int index)
+	{
+		return tcsComponents[index].sensor[0].FaultRate;
+	}
+}
+
+/*
+	Sending sensor 2 data from c++ to c#
+*/
+extern "C"
+{
+	__declspec(dllexport) int sendSens2Defined(int index)
+	{
+		return tcsComponents[index].sensor[1].IsDefined;
+	}
+	__declspec(dllexport) int sendSens2PID(int index)
+	{
+		return tcsComponents[index].sensor[1].SensorPIDNumber;
+	}
+	__declspec(dllexport) int sendSens2State(int index)
+	{
+		return tcsComponents[index].sensor[1].State;
+	}
+	__declspec(dllexport) int sendSens2OnlineVal(int index)
+	{
+		return tcsComponents[index].sensor[1].OnlineValue;
+	}
+	__declspec(dllexport) int sendSens2PrevVal(int index)
+	{
+		return tcsComponents[index].sensor[1].PreviousValue;
+	}
+	__declspec(dllexport) int sendSens2Fault(int index)
+	{
+		return tcsComponents[index].sensor[1].FaultRate;
+	}
+}
+
+/*
+	Sending sensor 3 data from c++ to c#
+*/
+extern "C"
+{
+	__declspec(dllexport) int sendSens3Defined(int index)
+	{
+		return tcsComponents[index].sensor[2].IsDefined;
+	}
+	__declspec(dllexport) int sendSens3PID(int index)
+	{
+		return tcsComponents[index].sensor[2].SensorPIDNumber;
+	}
+	__declspec(dllexport) int sendSens3State(int index)
+	{
+		return tcsComponents[index].sensor[2].State;
+	}
+	__declspec(dllexport) int sendSens3OnlineVal(int index)
+	{
+		return tcsComponents[index].sensor[2].OnlineValue;
+	}
+	__declspec(dllexport) int sendSens3PrevVal(int index)
+	{
+		return tcsComponents[index].sensor[2].PreviousValue;
+	}
+	__declspec(dllexport) int sendSens3Fault(int index)
+	{
+		return tcsComponents[index].sensor[2].FaultRate;
+	}
+}
+
+/*
+	Sending heater 1 data from c++ to c#
+*/
+extern "C"
+{
+	__declspec(dllexport) int sendHeater1Defined(int index)
+	{
+		return tcsComponents[index].heater[0].IsDefined;
+	}
+	__declspec(dllexport) int sendHeater1PID(int index)
+	{
+		return tcsComponents[index].heater[0].HeaterPIDNumber;
+	}
+	__declspec(dllexport) int sendHeater1State(int index)
+	{
+		return tcsComponents[index].heater[0].State;
+	}
+	__declspec(dllexport) int sendHeater1Status(int index)
+	{
+		return tcsComponents[index].heater[0].Status;
+	}
+}
+
+/*
+	Sending heater 2 data from c++ to c#
+*/
+extern "C"
+{
+	__declspec(dllexport) int sendHeater2Defined(int index)
+	{
+		return tcsComponents[index].heater[1].IsDefined;
+	}
+	__declspec(dllexport) int sendHeater2PID(int index)
+	{
+		return tcsComponents[index].heater[1].HeaterPIDNumber;
+	}
+	__declspec(dllexport) int sendHeater2State(int index)
+	{
+		return tcsComponents[index].heater[1].State;
+	}
+	__declspec(dllexport) int sendHeater2Status(int index)
+	{
+		return tcsComponents[index].heater[1].Status;
+	}
+
+}
+
+// Receiving data from C#
+/* Check bypassing conditions
+	This function check whether GUI/GS/OBC sends commands, if so, the algorithm must store new conditions
+	and use those values for the next time it initializes (resets). These parameters, currently, include
+	bypassing algorithm (AlgorithmBypass), obc/gs command (OBC) and heater start/stop temperatures.
+*/
+extern "C" __declspec(dllexport) void ReceiveDataFromGSorOBC(int Comp, bool CompStat, bool OBC, 
+	bool Bypass, int Sen1, int Sen2, int Sen3, int HeatStartOp, int HeatStopOp, int HeatStartNop, int HeatStopNop)
+{
+	tcsComponents[Comp].Status = CompStat;
+	tcsComponents[Comp].OBCCommand = OBC;
+	tcsComponents[Comp].OBC = Bypass;
+	tcsComponents[Comp].sensor[0].PreviousValue = tcsComponents[Comp].sensor[0].OnlineValue;
+	tcsComponents[Comp].sensor[1].PreviousValue = tcsComponents[Comp].sensor[1].OnlineValue;
+	tcsComponents[Comp].sensor[2].PreviousValue = tcsComponents[Comp].sensor[2].OnlineValue;
+	tcsComponents[Comp].sensor[0].OnlineValue = Sen1;
+	tcsComponents[Comp].sensor[1].OnlineValue = Sen2;
+	tcsComponents[Comp].sensor[2].OnlineValue = Sen3;
+	
+	tcsComponents[Comp].StartHeatOp = HeatStartOp;
+	tcsComponents[Comp].StopHeatOp = HeatStopOp;
+	tcsComponents[Comp].StartHeatNop = HeatStartNop;
+	tcsComponents[Comp].StopHeatNop = HeatStopNop;
+	
+	//backing up important data to tcsBackup
+	tcsBackup[Comp].Status = CompStat;
+	tcsBackup[Comp].OBCCommand = OBC;
+	tcsBackup[Comp].OBC = Bypass;
+	
+
+	tcsBackup[Comp].StartHeatOp = HeatStartOp;
+	tcsBackup[Comp].StopHeatOp = HeatStopOp;
+	tcsBackup[Comp].StartHeatNop = HeatStartNop;
+	tcsBackup[Comp].StopHeatNop = HeatStopNop;
+	
+}
+
+// Sending RID size
+extern "C" 
+{
+	__declspec(dllexport) int GetRIDSize()
+	{
+		int RIDSize = 0;
+		for (int comps = 0; comps < numRecords; comps++)
+		{
+			RIDSize += 3;	// Number of RIDs per component
+			for (int sensor = 0; sensor < MAX_NUMBER_SENSORS; sensor++)
+			{
+				if (tcsComponents[comps].sensor[sensor].IsDefined)
+				{
+					RIDSize += 1;	// Number of RIDs per sensor
+				}
+			}
+			for (int heater = 0; heater < MAX_NUMBER_HEATERS; heater++)
+			{
+				if (tcsComponents[comps].heater[heater].IsDefined)
+				{
+					RIDSize += 1;	//Number of RIDs per heater
+				}
+			}
+		}
+		return (RIDSize);
+	}
+}
+// defining RIDData vector globally
+int RIDsize = GetRIDSize();
+vector<int>		RIDData(RIDsize);
+
+void InitializeRID()
+{
+	fill(RIDData.begin(), RIDData.end(), -1);
+}
+
+//Sending RIDs
+/*
+Order of sending RIDs are as follows:
+1) Implausible for all components
+2) Operational limits for all components
+3) Non operational limits for all components
+4) faults of heaters
+5) faults of sensors
+Please sort your RIDs in compilance to the above format or otherwise change the rules according to your format.
+*/
+extern "C"
+{
+	__declspec(dllexport) int* GetRIDData()
+	{
+		int counter = 0;
+		for (int comps = 0; comps < numRecords; comps++)
+		{
+			if (tcsComponents[comps].RIDImpSend)
+			{
+				RIDData[counter] = tcsComponents[comps].RIDImplausible;
+			}
+			counter++;
+		}
+		
+		for (int comps = 0; comps < numRecords; comps++)
+		{
+			if (tcsComponents[comps].RIDOpSend)
+			{
+				RIDData[counter] = tcsComponents[comps].RIDOpLimit;
+			}
+			counter++;
+		}
+		for (int comps = 0; comps < numRecords; comps++)
+		{
+			if (tcsComponents[comps].RIDNopSend)
+			{
+				RIDData[counter] = tcsComponents[comps].RIDNopLimit;
+
+			}
+			counter++;
+		}
+		for (int comps = 0; comps < numRecords; comps++)
+		{
+			for (int heater = 0; heater < MAX_NUMBER_HEATERS; heater++)
+			{
+				if (tcsComponents[comps].heater[heater].IsDefined)
+				{
+					if (tcsComponents[comps].heater[heater].RIDSend)
+					{
+						RIDData[counter] = tcsComponents[comps].heater[heater].RID;
+					}
+					counter++;
+				}
+			}
+		}
+		for (int comps = 0; comps < numRecords; comps++)
+		{
+			for (int sensor = 0; sensor < MAX_NUMBER_SENSORS; sensor++)
+			{
+				if (tcsComponents[comps].sensor[sensor].IsDefined)
+				{
+					if (tcsComponents[comps].sensor[sensor].RIDSend)
+					{
+						RIDData[counter] = tcsComponents[comps].sensor[sensor].RID;
+					}
+					counter++;
+				}
+			}
+		}
+		return RIDData.data();
+	}
+}
+
+
+
+
+extern "C" __declspec(dllexport) int mainFunc()
 {
 	
 	
 	//Initializing components
-	cout << "initializing" << endl;
-	InitializeData(tcsComponents);
-	
+	//cout << "initializing" << endl;
+	InitializeData(tcsBackup);
+	tcsComponents = tcsBackup;
+	RIDsize = GetRIDSize();
+	RIDData.resize(RIDsize);
+	InitializeRID();
+	GetRIDData();
+
 	thread run_tcs([&]() {
 		while (true) {
+			
 			TCS(tcsComponents);
 			this_thread::sleep_for(chrono::seconds(TCS_RUN_INTERVAL));
 		}
 		});
 	thread reset_tcs([&]() {
 		while (true) {
-			cout << "TCS Reset." << endl;
-			InitializeData(tcsComponents);
+			//cout << "TCS Reset." << endl;
+			tcsComponents = tcsBackup;
+			
 			this_thread::sleep_for(chrono::seconds(TCS_RESET_TIME));
 		}
 		});
 
-	run_tcs.join();
 	reset_tcs.join();
+	run_tcs.join();
 	
 	
+	return 0;
 }
